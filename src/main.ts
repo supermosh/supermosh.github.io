35944cd91a6a0c4a950f4c5d706cfae19f83c54a
Nino Filiu <nino.filiu@gmail.com>
2021-06-03 19:00:33 +0200

    apply a simple transformation

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,11 @@
 
   const loop = () => {
     ctx.drawImage(video, 0, 0);
+    const imageData = ctx.getImageData(0, 0, width, height);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i] = 0;
+    }
+    ctx.putImageData(imageData, 0, 0);
     requestAnimationFrame(loop);
   };
   loop();
