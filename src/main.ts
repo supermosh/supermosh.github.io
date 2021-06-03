62688a55cd0a4ec0beccf176590052a48d864e57
Nino Filiu <nino.filiu@gmail.com>
2021-06-03 18:52:10 +0200

    capture video from webcam

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-const x: number = 10;
-console.log({ x });
+(async () => {
+  const video = document.querySelector('video');
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+  await new Promise<void>((resolve) => {
+    const listener = () => {
+      video.removeEventListener('canplay', listener);
+      resolve();
+    };
+    video.addEventListener('canplay', listener);
+  });
+  video.play();
+})();
