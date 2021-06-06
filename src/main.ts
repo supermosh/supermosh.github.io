ce72c2be7ea176297d17237bb6e08d3b44758372
Nino Filiu <nino.filiu@gmail.com>
2021-06-07 00:38:28 +0200

    record video by instrumenting it frame by frame

--- src/main.ts
+++ src/main.ts
@@ -1,95 +1,47 @@
-import approximate from './approximate';
-import getMinShifts from './getMinShifts';
-
-const size = 8;
-const shifts = [0, 1, -1, 2, -2, 4, -4, 8, -8];
-
-type State = {
-  recording: boolean;
-  transformation: (previous: ImageData, real: ImageData) => ImageData
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface HTMLVideoElement {
+  seekToNextFrame(): Promise<void>;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface HTMLCanvasElement {
+  captureStream(frameRate?: number): MediaStream;
 }
 
-const state: State = {
-  recording: false,
-  transformation: (previous, real) => real,
-};
-
 (async () => {
-  const video = document.createElement('video');
-  const inputStream = await navigator.mediaDevices.getUserMedia({ video: true });
-  video.srcObject = inputStream;
+  const video = document.querySelectorAll('video')[0];
+  video.src = '/static/small/hug-colors.mp4';
+
   await new Promise<void>((resolve) => {
     const listener = () => {
-      video.removeEventListener('canplay', listener);
+      video.removeEventListener('canplaythrough', listener);
       resolve();
     };
-    video.addEventListener('canplay', listener);
+    video.addEventListener('canplaythrough', listener);
   });
-  video.play();
 
-  const width = video.videoWidth;
-  const height = video.videoHeight;
   const canvas = document.querySelector('canvas');
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
   const ctx = canvas.getContext('2d');
 
-  const loop = () => {
-    const previous = ctx.getImageData(0, 0, width, height);
+  const stream = canvas.captureStream();
+  const recorder = new MediaRecorder(stream);
+  recorder.start();
+
+  const loop = async () => {
+    if (video.ended) {
+      recorder.stop();
+      return;
+    }
     ctx.drawImage(video, 0, 0);
-    const real = ctx.getImageData(0, 0, width, height);
-    const out = state.transformation(previous, real);
-    ctx.putImageData(out, 0, 0);
-    requestAnimationFrame(loop);
+    await video.seekToNextFrame();
+    setTimeout(loop, 1000 / 24);
   };
   loop();
 
-  // @ts-ignore
-  const outputStream = canvas.captureStream() as MediaStream;
-  const recorder = new MediaRecorder(outputStream, { mimeType: 'video/webm' });
-
-  const startRecording = () => {
-    state.recording = true;
-    recorder.start();
-  };
-
-  const stopRecording = () => {
-    state.recording = false;
-    recorder.addEventListener('dataavailable', (evt) => {
-      const url = URL.createObjectURL(evt.data);
-      const outputVideo = document.querySelector('video');
-      outputVideo.src = url;
-    });
-    recorder.stop();
-  };
-
-  document.addEventListener('keypress', (evt) => {
-    switch (evt.key) {
-      case 'r':
-        if (state.recording) {
-          stopRecording();
-        } else {
-          startRecording();
-        }
-        break;
-      case 'i':
-        state.transformation = (previous, real) => real;
-        break;
-      case 'a':
-        state.transformation = (previous, real) => {
-          const minShifts = getMinShifts(previous, real, size, shifts);
-          const out = approximate(previous, minShifts, size);
-          return out;
-        };
-        break;
-      case 'g':
-        state.transformation = (previous, real) => {
-          const minShifts = getMinShifts(previous, real, size, shifts);
-          const out = approximate(previous, minShifts, size);
-          state.transformation = (previous2) => approximate(previous2, minShifts, size);
-          return out;
-        };
-        break;
-    }
+  recorder.addEventListener('dataavailable', (evt) => {
+    const url = URL.createObjectURL(evt.data);
+    const outputVideo = document.querySelectorAll('video')[1];
+    outputVideo.src = url;
   });
 })();
