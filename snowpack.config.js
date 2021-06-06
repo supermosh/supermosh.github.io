45d690bbf8d104245404386796c791d72ffb039c
Nino Filiu <nino.filiu@gmail.com>
2021-06-07 00:38:28 +0200

    record video by instrumenting it frame by frame

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,9 +1,15 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: {
+      url: '/',
+      static: true,
+    },
+    src: {
+      url: '/dist',
+    },
   },
   plugins: [
-    '@snowpack/plugin-typescript'
-  ]
+    '@snowpack/plugin-typescript',
+  ],
 };
