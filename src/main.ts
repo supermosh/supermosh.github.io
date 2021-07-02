513412892d62baac60d9caed8b311f246abf065b
Nino Filiu <nino.filiu@gmail.com>
2021-07-02 22:23:05 +0200

    prepare segments

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,92 @@
-import approximate from './approximate';
+// import approximate from './approximate';
 import getMinShifts from './getMinShifts';
+import { MinShifts } from './types';
 
+const fps = 30;
 const size = 16;
 const shifts = [0, 1, -1, 2, -2, 4, -4, 8, -8];
 
+type BaseSegment = {src: string}
+type CopySegment = BaseSegment & {
+  transform: 'copy';
+  start: number;
+  end: number;
+};
+type PreparedCopySegment = CopySegment & {}
+type GlideSegment = BaseSegment & {
+  transform: 'glide';
+  time: number;
+  length: number;
+};
+type PreparedGlideSegment = GlideSegment & {
+  minShifts: MinShifts;
+}
+type MovementSegment = BaseSegment & {
+  transform: 'movement';
+  start: number;
+  end: number;
+}
+type PreparedMovementSegment = MovementSegment & {
+  minShiftss: MinShifts[];
+}
+type Segment = CopySegment | GlideSegment | MovementSegment;
+type PreparedSegment = PreparedCopySegment | PreparedGlideSegment | PreparedMovementSegment;
+
+const prepareGlideSegment = async (segment: GlideSegment): Promise<PreparedGlideSegment> => {
+  const video = document.createElement('video');
+  document.body.append(video);
+  video.src = segment.src;
+  await new Promise((resolve) => video.addEventListener('canplaythrough', resolve, { once: true }));
+
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  const canvas = document.createElement('canvas');
+  document.body.append(canvas);
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+
+  video.currentTime = segment.time;
+  await new Promise((resolve) => video.addEventListener('seeked', resolve, { once: true }));
+  ctx.drawImage(video, 0, 0);
+  const previous = ctx.getImageData(0, 0, width, height);
+  video.currentTime = segment.time + 1 / fps;
+  await new Promise((resolve) => video.addEventListener('seeked', resolve, { once: true }));
+  ctx.drawImage(video, 0, 0);
+  const real = ctx.getImageData(0, 0, width, height);
+
+  const minShifts = getMinShifts(previous, real, size, shifts);
+
+  video.remove();
+  canvas.remove();
+  return { ...segment, minShifts };
+};
+
+// TODO
+const prepareMovementSegment = async (segment: MovementSegment): Promise<PreparedMovementSegment> => ({ ...segment, minShiftss: [[]] });
+
 (async () => {
-  const video0 = document.querySelectorAll('video')[0];
-  video0.src = '/static/medium/face-colors.mp4';
-  const video1 = document.querySelectorAll('video')[1];
-  video1.src = '/static/medium/motocross.mp4';
-  await Promise.all([video0, video1].map((video) => new Promise((r) => video.addEventListener('canplaythrough', r, { once: true }))));
+  const segments: Segment[] = [
+    {
+      src: '/static/small/face-colors.mp4',
+      transform: 'copy',
+      start: 0,
+      end: 2,
+    },
+    {
+      src: '/static/small/face-colors.mp4',
+      transform: 'glide',
+      time: 2,
+      length: 4,
+    },
+  ];
 
-  const width = video0.videoWidth;
-  const height = video0.videoHeight;
-  const copyCanvas = document.querySelectorAll('canvas')[0];
-  copyCanvas.width = width;
-  copyCanvas.height = height;
-  const copyCtx = copyCanvas.getContext('2d');
-  const outCanvas = document.querySelectorAll('canvas')[1];
-  outCanvas.width = width;
-  outCanvas.height = height;
-  const outCtx = outCanvas.getContext('2d');
-
-  const minShifts1s = [];
-  while (video1.currentTime < 3) {
-    copyCtx.drawImage(video1, 0, 0);
-    const previous1 = copyCtx.getImageData(0, 0, width, height);
-    // @ts-ignore
-    await video1.seekToNextFrame();
-    copyCtx.drawImage(video1, 0, 0);
-    const real1 = copyCtx.getImageData(0, 0, width, height);
-    minShifts1s.push(getMinShifts(previous1, real1, size, shifts));
-  }
-
-  // @ts-ignore
-  const stream = outCanvas.captureStream() as MediaStream;
-  const recorder = new MediaRecorder(stream);
-  recorder.start();
-
-  let minShifts1Index = 0;
-  let i = 0;
-  const loop = async () => {
-    if (video0.currentTime < 3) {
-      outCtx.drawImage(video0, 0, 0);
-      // @ts-ignore
-      await video0.seekToNextFrame();
-    } else if (minShifts1Index < minShifts1s.length) {
-      const previous0 = outCtx.getImageData(0, 0, width, height);
-      const outData = approximate(previous0, minShifts1s[minShifts1Index], size);
-      outCtx.putImageData(outData, 0, 0);
-      minShifts1Index++;
-    } else if (i < 300) {
-      const previous0 = outCtx.getImageData(0, 0, width, height);
-      const outData = approximate(previous0, minShifts1s[minShifts1s.length - 1], size);
-      outCtx.putImageData(outData, 0, 0);
-      i++;
-    } else {
-      recorder.stop();
-      return;
+  const preparedSegments: PreparedSegment[] = await Promise.all(segments.map(async (segment) => {
+    switch (segment.transform) {
+      case 'copy': return segment;
+      case 'glide': return prepareGlideSegment(segment);
+      case 'movement': return prepareMovementSegment(segment);
     }
-    requestAnimationFrame(loop);
-  };
-  loop();
-
-  recorder.addEventListener('dataavailable', (evt) => {
-    const url = URL.createObjectURL(evt.data);
-    const outputVideo = document.querySelectorAll('video')[2];
-    outputVideo.src = url;
-  });
+  }));
 })();
