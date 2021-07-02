c4cfb923c6df811c7350d239e37059f73f413163
Nino Filiu <nino.filiu@gmail.com>
2021-07-02 17:47:52 +0200

    colors + motocross + pspam

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import approximate from './approximate';
 import getMinShifts from './getMinShifts';
 
-const size = 8;
-const shifts = [0, 1, -1, 2, -2, 4, -4];
+const size = 16;
+const shifts = [0, 1, -1, 2, -2, 4, -4, 8, -8];
 
 (async () => {
   const video0 = document.querySelectorAll('video')[0];
-  video0.src = '/static/small/face-colors.mp4';
+  video0.src = '/static/medium/face-colors.mp4';
   const video1 = document.querySelectorAll('video')[1];
-  video1.src = '/static/small/motocross.mp4';
+  video1.src = '/static/medium/motocross.mp4';
   await Promise.all([video0, video1].map((video) => new Promise((r) => video.addEventListener('canplaythrough', r, { once: true }))));
 
   const width = video0.videoWidth;
@@ -22,29 +22,39 @@ const shifts = [0, 1, -1, 2, -2, 4, -4];
   outCanvas.height = height;
   const outCtx = outCanvas.getContext('2d');
 
+  const minShifts1s = [];
+  while (video1.currentTime < 3) {
+    copyCtx.drawImage(video1, 0, 0);
+    const previous1 = copyCtx.getImageData(0, 0, width, height);
+    // @ts-ignore
+    await video1.seekToNextFrame();
+    copyCtx.drawImage(video1, 0, 0);
+    const real1 = copyCtx.getImageData(0, 0, width, height);
+    minShifts1s.push(getMinShifts(previous1, real1, size, shifts));
+  }
+
   // @ts-ignore
   const stream = outCanvas.captureStream() as MediaStream;
   const recorder = new MediaRecorder(stream);
   recorder.start();
 
+  let minShifts1Index = 0;
+  let i = 0;
   const loop = async () => {
     if (video0.currentTime < 3) {
       outCtx.drawImage(video0, 0, 0);
       // @ts-ignore
       await video0.seekToNextFrame();
-    } else if (video1.currentTime < 5) {
-      copyCtx.drawImage(video1, 0, 0);
-      const previous1 = copyCtx.getImageData(0, 0, width, height);
-      // @ts-ignore
-      await video1.seekToNextFrame();
-      copyCtx.drawImage(video1, 0, 0);
-      const real1 = copyCtx.getImageData(0, 0, width, height);
-      const minShifts1 = getMinShifts(previous1, real1, size, shifts);
-
+    } else if (minShifts1Index < minShifts1s.length) {
       const previous0 = outCtx.getImageData(0, 0, width, height);
-
-      const outData = approximate(previous0, minShifts1, size);
+      const outData = approximate(previous0, minShifts1s[minShifts1Index], size);
       outCtx.putImageData(outData, 0, 0);
+      minShifts1Index++;
+    } else if (i < 300) {
+      const previous0 = outCtx.getImageData(0, 0, width, height);
+      const outData = approximate(previous0, minShifts1s[minShifts1s.length - 1], size);
+      outCtx.putImageData(outData, 0, 0);
+      i++;
     } else {
       recorder.stop();
       return;
