b5729a7eb726b1bed37d3ebb9f413ea0c178c253
Nino Filiu <nino.filiu@gmail.com>
2021-07-08 00:24:41 +0200

    react-router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Studio from './Studio';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <div className="App">
+      <nav>
+        <Link to="/"><img src="/icons/home.svg" alt="home icon" /></Link>
+        <Link to="/studio"><img src="/icons/studio.svg" alt="studio icon" /></Link>
+        <Link to="/about"><img src="/icons/info.svg" alt="info icon" /></Link>
+      </nav>
+      <Switch>
+        <Route path="/studio"><Studio /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/"><Home /></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>,
   document.querySelector('#root'),
 );
