6b88610db6bc7c6b63816288cc496c6ac5600781
Nino Filiu <nino.filiu@gmail.com>
2021-07-08 22:57:37 +0200

    segments ui

--- src/Studio.tsx
+++ src/Studio.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Video } from './lib';
 import Videos from './Videos';
+import Segments from './Segments';
 
 const debug = true;
 
@@ -12,15 +13,25 @@ export default () => {
       const resp = await fetch('/media/bg.webm');
       const blob = await resp.blob();
       const file = new File([blob], 'bg.webm', { lastModified: +new Date() });
-      const url = URL.createObjectURL(file);
-      setVideos([{ file, url, previewing: false }]);
+      setVideos([
+        {
+          file,
+          url: URL.createObjectURL(file),
+          previewing: false,
+        },
+        {
+          file,
+          url: URL.createObjectURL(file),
+          previewing: false,
+        },
+      ]);
     })();
   }
 
   return (
     <div className="Studio">
       <Videos {...{ videos, setVideos }} />
-      <div className="" />
+      <Segments {...{ videos }} />
       <div className="" />
       <div className="" />
     </div>
