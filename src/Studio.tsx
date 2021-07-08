4c362b31718e8f54135a708b3c2042d12dc6010a
Nino Filiu <nino.filiu@gmail.com>
2021-07-08 22:14:31 +0200

    debug

--- src/Studio.tsx
+++ src/Studio.tsx
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 import { Video } from './lib';
 import Videos from './Videos';
 
+const debug = true;
+
 export default () => {
   const [videos, setVideos] = useState<Video[]>([]);
+
+  if (debug && videos.length === 0) {
+    (async () => {
+      const resp = await fetch('/media/bg.webm');
+      const blob = await resp.blob();
+      const file = new File([blob], 'bg.webm', { lastModified: +new Date() });
+      const url = URL.createObjectURL(file);
+      setVideos([{ file, url, previewing: false }]);
+    })();
+  }
 
   return (
     <div className="Studio">
