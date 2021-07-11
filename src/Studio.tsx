5991b87af87d816ca1998a2cd3e374ed8ee10375
Nino Filiu <nino.filiu@gmail.com>
2021-07-11 22:58:05 +0200

    setSrc

--- src/Studio.tsx
+++ src/Studio.tsx
@@ -13,23 +13,26 @@ export default () => {
     (async () => {
       const resp = await fetch('/media/bg.webm');
       const blob = await resp.blob();
-      const file = new File([blob], 'bg.webm', { lastModified: +new Date() });
+      const file0 = new File([blob], 'bg0.webm', { lastModified: +new Date() });
+      const url0 = URL.createObjectURL(file0);
+      const file1 = new File([blob], 'bg1.webm', { lastModified: +new Date() });
+      const url1 = URL.createObjectURL(file1);
       setVideos([
         {
-          file,
-          url: URL.createObjectURL(file),
+          file: file0,
+          url: url0,
           previewing: false,
         },
         {
-          file,
-          url: URL.createObjectURL(file),
+          file: file1,
+          url: url1,
           previewing: false,
         },
       ]);
       setSegments([
-        { transform: 'copy', src: 'bg.webm', start: 0, end: 1 },
-        { transform: 'glide', src: 'bg.webm', time: 1, length: 1 },
-        { transform: 'movement', src: 'bg.webm', start: 1, end: 2 },
+        { transform: 'copy', src: url0, start: 0, end: 1 },
+        { transform: 'glide', src: url0, time: 1, length: 1 },
+        { transform: 'movement', src: url1, start: 1, end: 2 },
       ]);
     })();
   }
