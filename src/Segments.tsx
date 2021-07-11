e078a684c437ed2892d40aa50b8373ef3673cd05
Nino Filiu <nino.filiu@gmail.com>
2021-07-11 22:38:04 +0200

    move up, move down, delete

--- src/Segments.tsx
+++ src/Segments.tsx
@@ -10,7 +10,21 @@ export default ({
   segments: Segment[],
   setSegments: Dispatch<SetStateAction<Segment[]>>
 }) => {
-  console.log({ videos, segments, setSegments });
+  const moveUp = (i: number) => {
+    segments.splice(i - 1, 2, segments[i], segments[i - 1]);
+    setSegments([...segments]);
+  };
+
+  const moveDown = (i: number) => {
+    segments.splice(i, 2, segments[i + 1], segments[i]);
+    setSegments([...segments]);
+  };
+
+  const remove = (i: number) => {
+    segments.splice(i, 1);
+    setSegments([...segments]);
+  };
+
   return (
     <div className="Segments">
       {videos.length === 0 ? (
@@ -26,6 +40,7 @@ export default ({
                   className="u-icon-button"
                   type="button"
                   style={{ visibility: i > 0 ? 'visible' : 'hidden' }}
+                  onClick={() => moveUp(i)}
                 >
                   <img src="/icons/up.svg" alt="" />
                 </button>
@@ -33,12 +48,14 @@ export default ({
                   className="u-icon-button"
                   type="button"
                   style={{ visibility: i < segments.length - 1 ? 'visible' : 'hidden' }}
+                  onClick={() => moveDown(i)}
                 >
                   <img src="/icons/down.svg" alt="" />
                 </button>
                 <button
                   className="u-icon-button"
                   type="button"
+                  onClick={() => remove(i)}
                 >
                   <img src="/icons/delete.svg" alt="" />
                 </button>
