be60cf799db8416196aa351f07f0ec9c319ccf71
Nino Filiu <nino.filiu@gmail.com>
2021-07-12 02:37:57 +0200

    render and result

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,12 @@ export type Video = {
   url: string;
   previewing: boolean;
 }
+export type Output = null | {
+  width: number;
+  height: number;
+  videoUrl: string;
+  videoSize: number;
+  videoType: string;
+  imageUrl: string;
+  imageSize: number;
+}
