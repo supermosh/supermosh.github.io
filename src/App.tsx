9bfdc7bf89ac0c35bdd66c3be7dd2496dd472959
Nino Filiu <nino.filiu@gmail.com>
2021-07-13 22:10:29 +0200

    extract app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Studio from './Studio';
+
+export default () => (
+  <BrowserRouter>
+    <div className="App">
+      <nav>
+        <Link to="/"><img src="/icons/home.svg" alt="home icon" /></Link>
+        <Link to="/studio"><img src="/icons/studio.svg" alt="studio icon" /></Link>
+        <Link to="/about"><img src="/icons/info.svg" alt="info icon" /></Link>
+      </nav>
+      <Switch>
+        <Route path="/studio"><Studio /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/"><Home /></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
