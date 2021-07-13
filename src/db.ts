6723978fa435c85b56c61c175313e8a5a8bbc3c3
Nino Filiu <nino.filiu@gmail.com>
2021-07-13 23:42:07 +0200

    filesStore

--- src/db.ts
+++ src/db.ts
@@ -16,34 +16,25 @@ export const setup = () => new Promise<void>((resolve, reject) => {
   };
 });
 
-export const stores = {
-  files: {
-    getAll: () => new Promise<SavedFile[]>((resolve, reject) => {
-      const op = db.transaction('files', 'readonly').objectStore('files').getAll();
-      op.onerror = reject;
-      op.onsuccess = () => resolve(op.result);
-    }),
-    add: (file: File) => new Promise<number>((resolve, reject) => {
-      const op = db.transaction('files', 'readwrite').objectStore('files').add({ file });
-      op.onerror = reject;
-      op.onsuccess = () => resolve(op.result as number);
-    }),
-    clear: () => new Promise<void>((resolve, reject) => {
-      const op = db.transaction('files', 'readwrite').objectStore('files').clear();
-      op.onerror = reject;
-      op.onsuccess = () => resolve();
-    }),
-    delete: (key: number) => new Promise<void>((resolve, reject) => {
-      const op = db.transaction('files', 'readwrite').objectStore('files').delete(key);
-      op.onerror = reject;
-      op.onsuccess = () => resolve();
-    }),
-  },
+export const filesStore = {
+  getAll: () => new Promise<SavedFile[]>((resolve, reject) => {
+    const op = db.transaction('files', 'readonly').objectStore('files').getAll();
+    op.onerror = reject;
+    op.onsuccess = () => resolve(op.result);
+  }),
+  add: (file: File) => new Promise<number>((resolve, reject) => {
+    const op = db.transaction('files', 'readwrite').objectStore('files').add({ file });
+    op.onerror = reject;
+    op.onsuccess = () => resolve(op.result as number);
+  }),
+  clear: () => new Promise<void>((resolve, reject) => {
+    const op = db.transaction('files', 'readwrite').objectStore('files').clear();
+    op.onerror = reject;
+    op.onsuccess = () => resolve();
+  }),
+  delete: (key: number) => new Promise<void>((resolve, reject) => {
+    const op = db.transaction('files', 'readwrite').objectStore('files').delete(key);
+    op.onerror = reject;
+    op.onsuccess = () => resolve();
+  }),
 };
-
-// @ts-ignore
-window.tx = (method, ...args) => new Promise((resolve, reject) => {
-  const request = db.transaction('files', 'readwrite').objectStore('files')[method](...args);
-  request.onerror = reject;
-  request.onsuccess = () => resolve(request.result);
-});
