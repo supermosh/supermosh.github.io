fd47139b00d3d64d353e22162a426a116d56ddec
Nino Filiu <nino.filiu@gmail.com>
2021-07-29 00:20:24 +0200

    fix TimeLengthInput

--- www/src/TimeLengthInput.tsx
+++ www/src/TimeLengthInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { GlideSegment } from 'supermosh';
 import Modal from './Modal';
 
@@ -12,35 +12,15 @@ export default ({
   const [editing, setEditing] = useState<boolean>(false);
   const [time, setTime] = useState<number>(segment.time);
   const [length, setLength] = useState<number>(segment.length);
-  const timeVideo = useRef<HTMLVideoElement>(null);
-  const loopVideo = useRef<HTMLVideoElement>(null);
-  const loopTimeoutId = useRef<number>(0);
+  const video = useRef<HTMLVideoElement>(null);
 
   const onTimeChange = (newTime: number) => {
     setTime(newTime);
-    timeVideo.current.currentTime = newTime;
-    if (newTime + length > loopVideo.current.duration) {
-      setLength(loopVideo.current.duration - newTime);
-    }
+    video.current.currentTime = newTime;
   };
 
   const onLengthChange = (newLength: number) => {
     setLength(newLength);
-    if (time + newLength > loopVideo.current.duration) {
-      setTime(loopVideo.current.duration - newLength);
-    }
-  };
-
-  const loop = () => {
-    if (!loopVideo.current) return;
-    if (time === loopVideo.current.duration || length === 0) {
-      loopVideo.current.currentTime = time;
-      loopVideo.current.pause();
-      return;
-    }
-    loopVideo.current.currentTime = time;
-    if (loopVideo.current.paused) loopVideo.current.play();
-    loopTimeoutId.current = setTimeout(loop, length * 1000);
   };
 
   const cancel = () => {
@@ -54,14 +34,8 @@ export default ({
     onChange(time, length);
   };
 
-  useEffect(() => {
-    if (!loopVideo.current) return;
-    if (loopTimeoutId.current) clearTimeout(loopTimeoutId.current);
-    loop();
-  }, [time, length, editing]);
-
   return (
-    <div className="RangeInput">
+    <div className="TimeLengthInput">
       <button
         type="button"
         className="u-normal-button display-button"
@@ -73,41 +47,32 @@ export default ({
       {editing && (
         <Modal onClose={() => setEditing(false)}>
           <div className="modal-content">
-            <div className="videos">
-              <div>
-                <div>
-                  <video src={segment.src} ref={timeVideo} />
-                </div>
-                <div>
-                  {`Time: ${time}s`}
-                </div>
-                <div>
-                  <input
-                    type="range"
-                    min="0"
-                    max={loopVideo.current?.duration}
-                    step="0.1"
-                    value={time}
-                    onChange={(evt) => onTimeChange(+evt.target.value)}
-                  />
-                </div>
-              </div>
-              <div>
-                <video src={segment.src} ref={loopVideo} muted />
-                <div>
-                  {`Length: ${length.toFixed(1)}s`}
-                </div>
-                <div>
-                  <input
-                    type="range"
-                    min="0"
-                    max={loopVideo.current ? (loopVideo.current.duration - time) : Infinity}
-                    step="0.1"
-                    value={length}
-                    onChange={(evt) => onLengthChange(+evt.target.value)}
-                  />
-                </div>
-              </div>
+            <video src={segment.src} ref={video} />
+            <div>
+              {`Time: ${time}s`}
+            </div>
+            <div>
+              <input
+                type="range"
+                min="0"
+                max={video.current?.duration}
+                step="0.1"
+                value={time}
+                onChange={(evt) => onTimeChange(+evt.target.value)}
+              />
+            </div>
+            <div>
+              {`Length: ${length.toFixed(1)}s`}
+            </div>
+            <div>
+              <input
+                type="range"
+                min="0"
+                max="10"
+                step="0.1"
+                value={length}
+                onChange={(evt) => onLengthChange(+evt.target.value)}
+              />
             </div>
             <div className="buttons">
               <button
