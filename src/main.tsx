98a06216ef8a9f3751be83927eaecc1bec12831a
Nino Filiu <nino.filiu@gmail.com>
2023-07-02 19:54:36 +0200

    router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
-import "./index.css";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              base layout
+              <Outlet />
+            </>
+          }
+        >
+          <Route index element={<div>index</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
