10c79556f6f3fc7a992026b207502fadfe3af6b3
Nino Filiu <nino.filiu@gmail.com>
2024-11-03 00:59:44 +0100

    tabula rasa

--- app/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./track";
-import App from "./App";
-import { setup } from "./db";
-
-(async () => {
-  await setup();
-  createRoot(document.querySelector("#root")!).render(<App />);
-})();
