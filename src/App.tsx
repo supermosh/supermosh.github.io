80002a169f48ff054f2e11dc32644eeac56cf8ce
Nino Filiu <nino.filiu@gmail.com>
2024-11-03 01:05:18 +0100

    setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,22 @@
-export const App = () => <></>;
+import { Link, Route, Routes } from "react-router-dom";
+
+export const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/studio">Studio</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+    <Routes>
+      <Route path="/" element={<>Home</>} />
+      <Route path="/studio" element={<>Studio</>} />
+      <Route path="/about" element={<>About</>} />
+    </Routes>
+  </>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
