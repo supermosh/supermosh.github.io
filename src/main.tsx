d925bcf1ff632f3f4b7b70b686d2867ec9524a17
Nino Filiu <nino.filiu@gmail.com>
2024-11-03 01:07:36 +0100

    lint imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+
+import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
