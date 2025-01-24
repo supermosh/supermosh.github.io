750b1b9049d945a294d4e24d13509e21915ac089
Nino Filiu <nino.filiu@gmail.com>
2025-01-24 16:35:42 +0100

    add home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { Link, Route, Routes } from "react-router-dom";
 
+import { Home } from "./Home";
+import { Icon } from "./Icon";
 import { Studio } from "./Studio";
 
 export const App = () => (
   <>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/studio">Studio</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-    <hr />
+    <nav>
+      <Link to="/" className="link-button">
+        <Icon name="home" /> Home
+      </Link>
+      <Link to="/studio" className="link-button">
+        <Icon name="movie" /> Studio
+      </Link>
+      <a
+        href="https://github.com/supermosh/supermosh.github.io"
+        className="link-button"
+      >
+        <Icon name="data_object" /> Github
+      </a>
+    </nav>
     <Routes>
-      <Route path="/" element={<>Home</>} />
+      <Route path="/" element={<Home />} />
       <Route path="/studio" element={<Studio />} />
-      <Route path="/about" element={<>About</>} />
     </Routes>
   </>
 );
