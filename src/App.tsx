673538df09e61faf4266294df0c6a3a6bf203548
Nino Filiu <2unfshwpu@mozmail.com>
2025-01-25 22:45:55 +0100

    add bug report button

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useMatch } from "react-router-dom";
 
-import { Home } from "./Home";
 import { Icon } from "./components/Icon";
+import { Home } from "./Home";
 import { Studio } from "./Studio";
 
-export const App = () => (
-  <>
-    <nav>
-      <Link to="/" className="link-button">
-        <Icon name="home" /> Home
-      </Link>
-      <Link to="/studio" className="link-button">
-        <Icon name="movie" /> Studio
-      </Link>
-      <a
-        href="https://github.com/supermosh/supermosh.github.io"
-        className="link-button"
-      >
-        <Icon name="data_object" /> Github
-      </a>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/studio" element={<Studio />} />
-    </Routes>
-  </>
-);
+export const App = () => {
+  const match = useMatch("/studio");
+  return (
+    <>
+      <nav>
+        <Link to="/" className="link-button">
+          <Icon name="home" /> Home
+        </Link>
+        <Link to="/studio" className="link-button">
+          <Icon name="movie" /> Studio
+        </Link>
+        <a
+          href="https://github.com/supermosh/supermosh.github.io"
+          className="link-button"
+        >
+          <Icon name="data_object" /> Github
+        </a>
+        {match && (
+          <a
+            href="https://github.com/supermosh/supermosh.github.io/issues"
+            className="link-button"
+          >
+            <Icon name="bug_report" /> Report a bug
+          </a>
+        )}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/studio" element={<Studio />} />
+      </Routes>
+    </>
+  );
+};
