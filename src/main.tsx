06f7926e3c0c3cb138a1203c45c77d75e05d4431
Nino Filiu <2unfshwpu@mozmail.com>
2025-06-14 16:27:50 +0200

    no need for future

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,7 @@ import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
