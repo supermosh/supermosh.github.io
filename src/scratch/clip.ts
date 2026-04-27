f2192bb5a5a91ba0ed197ca67609cf4b13c9bc70
Nino Filiu <filiunino@gmail.com>
2026-04-27 15:00:52 +0200

    oh yeah

--- src/scratch/clip.ts
+++ src/scratch/clip.ts
@@ -1,4 +1,5 @@
 import "../index.css";
+import "./clip.css";
 
 import {
   ALL_FORMATS,
@@ -16,15 +17,14 @@ import { x } from "./lib";
 
 let decoderConfig: VideoDecoderConfig;
 
-const extractPkts = async (mediaName: string) => {
+const extractPkts = async (name: string) => {
   const input = new Input({
     formats: ALL_FORMATS,
-    source: new UrlSource(`/samples/baseline/${mediaName}`),
+    source: new UrlSource(`/samples/baseline/${name}`),
   });
   const track = await input.getPrimaryVideoTrack();
   if (!track) throw new Error("no track");
   decoderConfig = x(await track.getDecoderConfig());
-  console.log(decoderConfig);
   if (!decoderConfig) throw new Error("can't decode");
   const sink = new EncodedPacketSink(track);
   const pkts: EncodedPacket[] = [];
@@ -36,12 +36,28 @@ const extractPkts = async (mediaName: string) => {
 
 console.log("Extracting packets...");
 
-const mediaNames = ["IMG_5424.mp4", "IMG_5425.mp4"] as const;
+const mediaNames = ["IMG_5432.mp4"] as const;
 type MediaName = (typeof mediaNames)[number];
 const pktss = {} as Record<MediaName, EncodedPacket[]>;
-for (const mediaName of mediaNames) {
-  pktss[mediaName as MediaName] = await extractPkts(mediaName);
-  console.log(`${mediaName}: ${pktss[mediaName].length} packets`);
+for (const name of mediaNames) {
+  const pkts = await extractPkts(name);
+  pktss[name as MediaName] = pkts;
+  const keyIndices = pkts
+    .map((pkt, i) => ({ pkt, i }))
+    .filter(({ pkt }) => pkt.type === "key")
+    .map(({ i }) => i);
+  const durations = new Set(pkts.map((pkt) => pkt.duration));
+  console.log(
+    `${name}: ${pktss[name].length} packets, keyframes: ${keyIndices}, durations: ${[...durations]}`,
+  );
+
+  const video = document.createElement("video");
+  video.src = `/samples/baseline/${name}`;
+  video.controls = true;
+  document.body.append(video);
+  video.addEventListener("timeupdate", () => {
+    console.log((pkts.length * video.currentTime) / video.duration);
+  });
 }
 
 console.log("Building timeline...");
@@ -55,16 +71,20 @@ const retimers = {
     Array(duration)
       .fill(null)
       .map(() => at),
-};
-type Timeline = { mediaName: MediaName; indices: number[] }[];
-const timeline: Timeline = [
-  {
-    mediaName: "IMG_5424.mp4",
-    indices: retimers.copy(0, 78),
+  stretch: (from: number, to: number, rate: number) => {
+    const length = Math.floor((to - from) / rate);
+    return Array(length)
+      .fill(null)
+      .map((_, i) => Math.floor(from + i * rate));
   },
+};
+type Timeline = { name: MediaName; indices: number[] }[];
+const timeline: Timeline = [
+  { name: "IMG_5432.mp4", indices: retimers.stretch(0, 59, 0.25) },
 ];
+console.log(timeline);
 const repkts = timeline
-  .flatMap(({ mediaName, indices }) => indices.map((i) => pktss[mediaName][i]))
+  .flatMap(({ name, indices }) => indices.map((i) => pktss[name][i]))
   .filter((pkt, i) => i == 0 || pkt.type == "delta")
   .map((pkt, i) => {
     return new EncodedPacket(
@@ -100,7 +120,6 @@ video.controls = true;
 video.loop = true;
 video.muted = true;
 video.autoplay = true;
-video.style.width = "100%";
 video.src = URL.createObjectURL(new Blob([output.target.buffer!]));
 document.body.append(video);
 
