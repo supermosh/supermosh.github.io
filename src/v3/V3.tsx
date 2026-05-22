122099e0aae63514ae0097ab5bce596b35148a11
Nino Filiu <filiunino@gmail.com>
2026-05-22 18:16:08 +0200

    proper files

--- src/v3/V3.tsx
+++ src/v3/V3.tsx
@@ -15,6 +15,11 @@ import { useState } from "react";
 
 import { x } from "../scratch/lib";
 
+type Media = {
+  name: string;
+  output: Output<Mp4OutputFormat, BufferTarget>;
+};
+
 export const V3 = () => {
   const [videoSrc, setVideoSrc] = useState("");
 
@@ -103,9 +108,80 @@ export const V3 = () => {
     setVideoSrc(URL.createObjectURL(new Blob([output.target.buffer!])));
   };
 
+  const [medias, setMedias] = useState<Media[]>([]);
+  const [isConverting, setIsConverting] = useState(false);
+  const [conversionProgress, setConversionProgress] = useState(0);
+  const onUpload = async (
+    evt: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) => {
+    const [file] = evt.target.files ?? [];
+    if (!file) {
+      console.warn("Should upload at least one file");
+      return;
+    }
+
+    setIsConverting(true);
+    setConversionProgress(0);
+
+    let newName = file.name;
+    let newNameSuffix = 1;
+    while (medias.some((upload) => upload.name === newName)) {
+      newName = `${file.name}_${newNameSuffix}`;
+      newNameSuffix++;
+    }
+
+    // convert
+    const width = 960;
+    const height = 540;
+    const convInput = new Input({
+      formats: ALL_FORMATS,
+      source: new BlobSource(file),
+    });
+    const convOutput = new Output({
+      format: new Mp4OutputFormat(),
+      target: new BufferTarget(),
+    });
+    const conversion = await Conversion.init({
+      input: convInput,
+      output: convOutput,
+      video: {
+        width,
+        height,
+        fit: "cover",
+        forceTranscode: true,
+        codec: "avc",
+        // Only works for a custom build of mediabunny, else is ignored and we hope for the best
+        fullCodecString: "avc1.42c01f",
+      },
+    });
+    if (!conversion.isValid) throw new Error("conv is not valid");
+    conversion.onProgress = setConversionProgress;
+    await conversion.execute();
+
+    evt.target.value = "";
+    setMedias([...medias, { name: newName, output: convOutput }]);
+    setIsConverting(false);
+  };
+
   return (
     <>
-      <input type="file" accept="video/*" onChange={scratch} />
+      <h1>Files</h1>
+      <ul>
+        {medias.map((upload) => (
+          <li key={upload.name}>{upload.name}</li>
+        ))}
+        <li>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={onUpload}
+            disabled={isConverting}
+          />
+          {isConverting &&
+            `Converting... (${Math.floor(100 * conversionProgress)}%)`}
+        </li>
+      </ul>
+
       {videoSrc && <video src={videoSrc} controls autoPlay muted loop></video>}
     </>
   );
