07b34e8ca715e315c02968eb71f0d2e2dec65b67
Nino Filiu <filiunino@gmail.com>
2026-05-22 17:39:25 +0200

    scratch convert

--- src/v3/V3.tsx
+++ src/v3/V3.tsx
@@ -1,3 +1,53 @@
+import {
+  ALL_FORMATS,
+  BlobSource,
+  BufferTarget,
+  Conversion,
+  Input,
+  Mp4OutputFormat,
+  Output,
+} from "mediabunny";
+
 export const V3 = () => {
-  return <></>;
+  const scratch = async (
+    evt: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) => {
+    const [file] = evt.target.files ?? [];
+    if (!file) throw new Error("Should be a file");
+    console.log({ file });
+
+    // convert
+    const width = 960;
+    const height = 540;
+    const convInput = new Input({
+      formats: ALL_FORMATS,
+      source: new BlobSource(file),
+    });
+    const convOutput = new Output({
+      format: new Mp4OutputFormat(),
+      target: new BufferTarget(),
+    });
+    const conversion = await Conversion.init({
+      input: convInput,
+      output: convOutput,
+      video: {
+        width,
+        height,
+        fit: "cover",
+        forceTranscode: true,
+        codec: "avc",
+        // Only works for a custom build of mediabunny, else is ignored and we hope for the best
+        fullCodecString: "avc1.42c01f",
+      },
+    });
+    if (!conversion.isValid) throw new Error("conv is not valid");
+    conversion.onProgress = (n) => console.log(`conv progress ${~~(100 * n)}%`);
+    await conversion.execute();
+  };
+
+  return (
+    <>
+      <input type="file" accept="video/*" onChange={scratch} />
+    </>
+  );
 };
